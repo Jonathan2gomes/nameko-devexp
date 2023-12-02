bd2942f534f06238d3fbb6c522abe63c0893848c
Jonathan Gomes <jonathan2gomes.jg@gmail.com>
2023-12-02 00:19:52 -0300

    - Adding smoke tests for deleting product call

--- test/nex-smoketest.sh
+++ test/nex-smoketest.sh
@@ -52,3 +52,11 @@ ID=$(echo ${ORDER_ID} | jq '.id')
 # Test: Get Order back
 echo "=== Getting Order ==="
 curl -s "${STD_APP_URL}/orders/${ID}" | jq .
+
+# Test: Delete Product
+echo "=== Deleting product id: the_odyssey ==="
+curl -s -X DELETE "${STD_APP_URL}/products/the_odyssey" | jq .
+
+# Test: Delete nonexistent Product
+echo "=== Deleting product nonexistent product ==="
+curl -s -X DELETE "${STD_APP_URL}/products/nonexistent_product" | jq .
